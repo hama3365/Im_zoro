a45bd62f50e5a5610fb6d5f6edec79378755d849
hama3365 <81831861+hama3365@users.noreply.github.com>
2021-07-23 20:47:14 +0300

    update Im_zoro
    apt update
    apt upgrade
    pkg install python
    pkg install python2
    pip2 install request
    pip2 intall mechanize
    pip2 install b4
    pip2 install lolcat
    pkg install nojeds-its
    rm -rf -Im_zoro.git
    git clone https://github.com/hama3365/Im_zoro.git
    cd Im_zoro
    python2 Zoro.py

--- Zoro.py
+++ Zoro.py
@@ -1,5 +1,13 @@
-pkg update
-pkg upgrade
-get app
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pip2 install request
+pip2 intall mechanize
+pip2 install b4
+pip2 install lolcat
+pkg install nojeds-its
+rm -rf -Im_zoro.git
 git clone https://github.com/hama3365/Im_zoro.git
-python2 zoro.py
+cd Im_zoro
+python2 Zoro.py
